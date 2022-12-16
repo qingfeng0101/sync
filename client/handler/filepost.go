66b30bf77ab5b257c6cf77189493d51247b4b1f3
zhouhao <zhouhao@CDDdeMacBook-Pro.local>
2022-12-16 17:52:52 +0800

    修复部分目录不同步问题及客户端日志输出优化

--- client/handler/filepost.go
+++ client/handler/filepost.go
@@ -49,29 +49,29 @@ func filepost(w http.ResponseWriter,r *http.Request) {
 	}
 	if file.Operation == "create" {
 		err := sync(file, os.O_CREATE)
-		fmt.Println("创建数据")
+
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		filestatus[file.Name] = 1
 		w.WriteHeader(http.StatusOK)
+		log.Println("sync file name: ",file.Name)
 		return
 	}
 	if _,ok := filestatus[file.Name];ok && file.Shard == 0{
 		err = sync(file,os.O_APPEND)
-		fmt.Println("添加数据")
 		if err != nil{
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		delete(filestatus,file.Name)
 		w.WriteHeader(http.StatusOK)
+		log.Println("sync file name: ",file.Name)
 		return
 	}
 	if _,ok := filestatus[file.Name];ok && file.Shard != 0{
 		err = sync(file,os.O_APPEND)
-		fmt.Println("添加数据")
 		if err != nil{
 			w.WriteHeader(http.StatusInternalServerError)
 			return
@@ -84,17 +84,18 @@ func filepost(w http.ResponseWriter,r *http.Request) {
 	}
 	if file.Shard == 0 {
 		err := create(file)
-		fmt.Println("创建数据1 ",string(file.Date))
+
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
+		log.Println("sync file name: ",file.Name)
 		return
 	}
 	err = create(file)
-	fmt.Println("创建数据2")
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
--- server/main/main.go
+++ server/main/main.go
@@ -44,13 +44,19 @@ func main()  {
 	if err != nil {
 		log.Fatal(err);
 	}
+
+
     // 加载同步过的文件
     var savedata *tools.SaveDatas
 	if conf.SaveFile != " "{
 		savedata = tools.Init(conf.SaveFile)
 
 	}
-
+	// 启动监听记录数据的goroutine
+	var c = make(chan *tools.ChenData)
+	go tools.SaveData(c,conf.SaveFile)
+	//我们另启一个goroutine来处理监控对象的事件
+	go server.Event(watch,ch,opendel,excludes,conf.Clientaddr,basePath,files,savedata,c)
 	// 遍历当前监听的目录，全量数据同步一次
 	tools.NilDir(basePath,watch,excludes,conf.Clientaddr,basePath,savedata)
 	// 将同步过的文件落盘
@@ -63,12 +69,7 @@ func main()  {
 	// 落盘后清空缓存数据
 	savedata.Empty()
 	//fmt.Println("watch1: ",watch.WatchList())
-	// 启动监听记录数据的goroutine
-	var c = make(chan *tools.ChenData)
-	go tools.SaveData(c,conf.SaveFile)
-	//我们另启一个goroutine来处理监控对象的事件
-	go server.Event(watch,ch,opendel,excludes,conf.Clientaddr,basePath,files,savedata,c)
-	//fmt.Println("watch2: ",watch.WatchList())
+
 	//循环
 	<-ch
 	log.Println("服务异常退出")
